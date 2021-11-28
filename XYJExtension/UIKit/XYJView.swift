5bd247fdc2d28d27b5e0ea80fdcfe92b8b3f0cdb
xiaoyingjun <445257607@qq.com>
2021-11-29 00:15:45 +0800

    View+ViewController

---
+++ XYJExtension/UIKit/XYJView.swift
@@ -0,0 +1,12 @@
+//
+//  XYJView.swift
+//  XYJExtension
+//
+//  Created by 肖迎军 on 2021/11/29.
+//
+
+import UIKit
+
+open class XYJView: UIView {
+
+}
---
+++ XYJExtension/UIKit/XYJViewController.swift
@@ -0,0 +1,12 @@
+//
+//  XYJViewController.swift
+//  XYJExtension
+//
+//  Created by 肖迎军 on 2021/11/29.
+//
+
+import UIKit
+
+open class XYJViewController: UIViewController {
+
+}
