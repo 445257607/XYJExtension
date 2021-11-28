37ba49b324bf44775bdb1e3840c68592d95cfa34
xiaoyingjun <445257607@qq.com>
2021-11-28 22:15:05 +0800

    init

---
+++ XYJExtension/XYJExtension.h
@@ -0,0 +1,18 @@
+//
+//  XYJExtension.h
+//  XYJExtension
+//
+//  Created by 肖迎军 on 2021/11/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XYJExtension.
+FOUNDATION_EXPORT double XYJExtensionVersionNumber;
+
+//! Project version string for XYJExtension.
+FOUNDATION_EXPORT const unsigned char XYJExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYJExtension/PublicHeader.h>
+
+
